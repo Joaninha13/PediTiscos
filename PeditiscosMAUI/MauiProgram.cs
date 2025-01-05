41e9a878e2542cd7426d107c10505786f994f406
Joaninha13 <66388508+Joaninha13@users.noreply.github.com>
2025-01-05 14:52:45 +0000

    Maui geral not done

--- PeditiscosMAUI/MauiProgram.cs
+++ PeditiscosMAUI/MauiProgram.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using RCLAPI.Services;
+using RCLComum.State;
+using RCLProdutos.Services.Interfaces;
+using RCLProdutos.Services;
 
 namespace PeditiscosMAUI
 {
@@ -14,10 +18,27 @@ namespace PeditiscosMAUI
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 });
 
+            //// Add services to the container.
+            //builder.Services.AddRazorComponents()
+            //    .AddInteractiveServerComponents();
+
             builder.Services.AddMauiBlazorWebView();
 
+            builder.Services.AddHttpClient();
+
+            // Register IHttpContextAccessor
+            builder.Services.AddHttpContextAccessor();
+
+            //Descomentar depois
+            builder.Services.AddScoped<ISliderUtilsServices, SliderUtilsServices>();
+            builder.Services.AddScoped<ICardsUtilsServices, CardsUtilsServices>();
+            builder.Services.AddScoped<IApiServices, ApiService>();
+            builder.Services.AddSingleton<UserSessionState>();
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7213") }); //NECESSARIO????
+
 #if DEBUG
-    		builder.Services.AddBlazorWebViewDeveloperTools();
+            builder.Services.AddBlazorWebViewDeveloperTools();
     		builder.Logging.AddDebug();
 #endif
 
